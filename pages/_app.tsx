43ca50aa2e68673f168b8131a303de03fbcc4400
reza <reza16.sugiarto@gmail.com>
2025-11-03 01:40:36 +0700

    Add icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@/styles/globals.css';
 import 'katex/dist/katex.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
