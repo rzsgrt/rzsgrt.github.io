26ee8babd919e3fd60bfeb06b1f416f96de86223
reza <reza16.sugiarto@gmail.com>
2025-11-03 01:57:11 +0700

    Change icon and blockquote

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
